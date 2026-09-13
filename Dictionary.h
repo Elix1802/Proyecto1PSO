f0c7241f10ed3bb8705b627fcf3d54bab9f96bcf
Elix1802 <elieceresquivel71@gmail.com>
2026-09-13 03:06:02 -0600

    aa

--- Dictionary.h
+++ Dictionary.h
@@ -4,11 +4,12 @@
 typedef struct KeyValue KeyValue;
 typedef struct Dictionary Dictionary;
 
-Dictionary * createDictiory();
+Dictionary * createDictionary();
 void destroyDictionary(Dictionary * self);
 
 //setters;
 void addDictionaryElement(Dictionary * self, char c, char * value);
 char * getDictionaryValue(Dictionary * self, char c);
+void printDictionaryValues(Dictionary * self);
 
 #endif
