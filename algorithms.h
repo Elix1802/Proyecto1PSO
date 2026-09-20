be64106c7a88046cce5fc46a831950d2f53724c8
Elix1802 <164799782+Elix1802@users.noreply.github.com>
2026-09-20 01:44:40 -0600

    Update include paths for data structures

--- algorithms.h
+++ algorithms.h
@@ -2,10 +2,10 @@
 #define ALGORITHMS_H
 
 #include <stdio.h>
-#include "Node.h"
-#include "HeapPriorityQueue.h"
-#include "HashTableFreq.h"
-#include "Dictionary.h"
+#include "dataStructs/Node.h"
+#include "dataStructs/HeapPriorityQueue.h"
+#include "dataStructs/HashTableFreq.h"
+#include "dataStructs/Dictionary.h"
 
 // -------------------------------------------------------------------
 // Estructuras
