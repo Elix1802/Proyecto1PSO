ede74afcbb8e753a7310b470108ab2bd1ad76595
Elix1802 <elieceresquivel71@gmail.com>
2026-09-20 18:12:43 -0600

    Print en el main.c

--- main.c
+++ main.c
@@ -195,7 +195,7 @@ static void on_decompress_button_clicked(GtkButton *button, gpointer user_data) 
         
         StatRecord* record = decompressAllFiles(selected_directory);
 
-        g_print("[Hijo %d] Compresión completada.\n", getpid());
+        g_print("[Salud %f].\n", record->healthPercentage);
         
         _exit(0);
 
